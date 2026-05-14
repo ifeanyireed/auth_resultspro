7344eb5661c034f89f8098fa475f62ebe96c36b9
iibeh <ifeanyireed@gmail.com>
2026-05-14 11:28:14 +0100

    Refactor: Switch to Resend API for email delivery to bypass DO SMTP blocks

--- utils/email.go
+++ utils/email.go
@@ -1,37 +1,70 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"net/smtp"
+	"net/http"
 	"os"
 )
 
-func SendEmail(to string, subject string, body string) error {
-	from := os.Getenv("SMTP_FROM")
-	password := os.Getenv("SMTP_PASS")
-	user := os.Getenv("SMTP_USER")
-	host := os.Getenv("SMTP_HOST")
-	port := os.Getenv("SMTP_PORT")
+type ResendPayload struct {
+	From    string `json:"from"`
+	To      []string `json:"to"`
+	Subject string   `json:"subject"`
+	Html    string   `json:"html"`
+}
 
-	auth := smtp.PlainAuth("", user, password, host)
+func SendEmail(to string, subject string, htmlBody string) error {
+	apiKey := os.Getenv("RESEND_API_KEY")
+	from := os.Getenv("SMTP_FROM") // We'll keep this variable name for consistency or rename it
 
-	msg := []byte("To: " + to + "\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"\r\n" +
-		body + "\r\n")
+	if apiKey == "" {
+		return fmt.Errorf("RESEND_API_KEY is not set")
+	}
 
-	addr := fmt.Sprintf("%s:%s", host, port)
-	return smtp.SendMail(addr, auth, from, []string{to}, msg)
+	payload := ResendPayload{
+		From:    from,
+		To:      []string{to},
+		Subject: subject,
+		Html:    htmlBody,
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", "https://api.resend.com/emails", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("resend api returned non-ok status: %d", resp.StatusCode)
+	}
+
+	return nil
 }
 
 func SendVerificationEmail(to string, token string) error {
 	subject := "Verify your email - ResultsPro"
-	body := fmt.Sprintf("Please verify your email by clicking the link below:\r\nhttps://auth.resultspro.ng/verify-email?token=%s", token)
+	body := fmt.Sprintf("<p>Please verify your email by clicking the link below:</p><p><a href=\"https://auth.resultspro.ng/verify-email?token=%s\">Verify Email</a></p>", token)
 	return SendEmail(to, subject, body)
 }
 
 func SendPasswordResetEmail(to string, token string) error {
 	subject := "Reset your password - ResultsPro"
-	body := fmt.Sprintf("Reset your password by clicking the link below:\r\nhttps://auth.resultspro.ng/reset-password?token=%s", token)
+	body := fmt.Sprintf("<p>Reset your password by clicking the link below:</p><p><a href=\"https://auth.resultspro.ng/reset-password?token=%s\">Reset Password</a></p>", token)
 	return SendEmail(to, subject, body)
 }
