c9b756eefce00439a03c471bddd426980292017a
Ifeanyi Felix <ifeanyireed@gmail.com>
2026-05-15 10:57:16 +0100

    Updated seed_central_auth.sql

--- seed_central_auth.sql
+++ seed_central_auth.sql
@@ -7,8 +7,8 @@
 INSERT OR REPLACE INTO users (id, email, password_hash, auth_provider, full_name, phone, sex, date_of_birth, address, account_status)
 VALUES 
 -- Original Accounts
-('bfb51c68-ccb0-401f-b58f-27fd41c6a856', 'superadmin@classroompro.com', '$2a$14$1zhGRoc.lxuxyO/9X27HpuUTq06m5p2pb69PgYa0UWksEJWT7kS8i', 'local', 'Super Admin', NULL, NULL, NULL, NULL, 'active'),
-('2db093ed-bdc9-47c4-b71c-66869f0f1ea7', 'teacher@lekkibritish.com', '$2a$14$Jg0JSBXO09zmMOssPyzEj.VyO/iuXai.QCZQFicC4CTR.plVD9dMS', 'local', 'Mr. Adeniyi', NULL, NULL, NULL, NULL, 'active'),
+('bfb51c68-ccb0-401f-b58f-27fd41c6a856', 'superadmin@esultspro.ng', '$2a$14$1zhGRoc.lxuxyO/9X27HpuUTq06m5p2pb69PgYa0UWksEJWT7kS8i', 'local', 'Super Admin', NULL, NULL, NULL, NULL, 'active'),
+('2db093ed-bdc9-47c4-b71c-66869f0f1ea7', 'teacher@example.edu', '$2a$14$Jg0JSBXO09zmMOssPyzEj.VyO/iuXai.QCZQFicC4CTR.plVD9dMS', 'local', 'Mr. Adeniyi', NULL, NULL, NULL, NULL, 'active'),
 ('111efa7d-e12d-4ed1-9902-d341c6826b50', 'student@example.com', '$2a$14$OiOxIN4UiEuFHKIhwdmFHuNbtI2FoVpU95KVD8Dc3FxLhHM2.EMve', 'local', 'Jane Doe', NULL, NULL, NULL, NULL, 'active'),
 ('dac38ffd-866f-47ab-8ac4-ecf6ea520ba8', 'parent@example.com', '$2a$14$4nofWUGNaOyx9/2zF23ySuu5ehgcPa1kApyvp5dLAHszuA.NoLOWS', 'local', 'Mrs. Doe', NULL, NULL, NULL, NULL, 'active'),
 -- Requested Admin/Support Accounts
